f7147c62182c6904866453785948bcbe34eae729
unknown <ykim164@illinois.edu>
2019-04-14 15:31:35 -0500

    install devextreme scheduler package

--- breaking_bad/web_app/src/components/Calendar/index.js
+++ breaking_bad/web_app/src/components/Calendar/index.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
-import './styles.css';
+import React from "react";
+import { render } from "react-dom";
+import Paper from "@material-ui/core/Paper";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  WeekView,
+  Appointments
+} from "@devexpress/dx-react-scheduler-material-ui";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+import { appointments } from "./data";
 
-class Calendar extends Component {
-    constructor(props) {
-        super(props);
-    }
+const theme = createMuiTheme({ palette: { type: "light", primary: blue } });
 
-    render() {
-        return (
-            <div class="row center">
-                <h2>Calendar Page Placeholder</h2>
-            </div>
+class Calendar extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      data: appointments,
+      currentDate: new Date(),
+    };   
+  }
+  render() {
+    const { data, currentDate } = this.state;
 
-        );
-    }
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Paper>
+          <Scheduler data={data}>
+            <ViewState currentDate={currentDate} />
+            <WeekView startDayHour={9} endDayHour={19} />
+            <Appointments />
+          </Scheduler>
+        </Paper>
+      </MuiThemeProvider>
+    );
+  }
 }
+
 export default Calendar;
