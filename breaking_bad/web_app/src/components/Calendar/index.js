3f53c98b58f623a634b311e2406711f86afd1fc4
unknown <ykim164@illinois.edu>
2019-04-14 15:49:40 -0500

    make calendar editable

--- breaking_bad/web_app/src/components/Calendar/index.js
+++ breaking_bad/web_app/src/components/Calendar/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import Paper from "@material-ui/core/Paper";
-import { ViewState } from "@devexpress/dx-react-scheduler";
+import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
 import {
-  Scheduler,
-  WeekView,
-  Appointments
-} from "@devexpress/dx-react-scheduler-material-ui";
+    Scheduler,
+    WeekView,
+    DayView,
+    Appointments,
+    AppointmentForm,
+    AppointmentTooltip,
+  } from '@devexpress/dx-react-scheduler-material-ui';
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { blue } from "@material-ui/core/colors";
 import { appointments } from "./data";
@@ -21,7 +24,33 @@ class Calendar extends React.PureComponent {
       data: appointments,
       currentDate: new Date(),
     };   
+    this.commitChanges = this.commitChanges.bind(this);
   }
+
+  // code from the documemtation: https://devexpress.github.io/devextreme-reactive/react/scheduler/docs/guides/editing/
+  commitChanges({ added, changed, deleted }) {
+    let { data } = this.state;
+    if (added) {
+      const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+      data = [
+        ...data,
+        {
+          id: startingAddedId,
+          ...added,
+        },
+      ];
+    }
+    if (changed) {
+      data = data.map(appointment => (
+        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+    }
+    if (deleted) {
+      data = data.filter(appointment => appointment.id !== deleted);
+    }
+    this.setState({ data });
+  }
+
+
   render() {
     const { data, currentDate } = this.state;
 
@@ -30,8 +59,14 @@ class Calendar extends React.PureComponent {
         <Paper>
           <Scheduler data={data}>
             <ViewState currentDate={currentDate} />
+            <EditingState onCommitChanges={this.commitChanges} />
             <WeekView startDayHour={9} endDayHour={19} />
             <Appointments />
+            <AppointmentTooltip
+            showOpenButton
+            showDeleteButton
+            />
+          <AppointmentForm />
           </Scheduler>
         </Paper>
       </MuiThemeProvider>
