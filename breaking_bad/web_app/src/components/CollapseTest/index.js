2894abcdb00c603950db8662f93994813c600c57
unknown <ykim164@illinois.edu>
2019-04-18 16:50:31 -0500

    refactor filter by date in feedback

--- breaking_bad/web_app/src/components/CollapseTest/index.js
+++ breaking_bad/web_app/src/components/CollapseTest/index.js
@@ -18,36 +18,20 @@ class CollpaseTest extends Component {
     }
 
     componentDidMount = () => {
+        // load data from DB
         let self = this
         let database = fire.database().ref();
         let userId = this.state.userId
         database.on("value", function (snapshot) {
             let scheduleArr = snapshot.val()[userId].schedule
-
-
             self.setState({
                 schedules: scheduleArr,
             })
-
-            // filter by current date (chosen date)
-            let filteredSchedule = []
-
-            for (let idx in self.state.schedules) {
-                if (new Date(self.state.schedules[idx].startDate).getDate() === self.state.selectedDate.getDate()) {
-                    filteredSchedule.push(self.state.schedules[idx])
-                }
-            }
-            
-            self.setState({
-                todaySchedules: filteredSchedule,
-            })
-
         })
     }
 
     handleFeedbackChange = (e, idx) => {
         e.preventDefault();
-
         let newState = Object.assign({}, this.state.schedules)
         newState[idx].feedback = e.target.value
         this.setState(newState)
@@ -56,13 +40,11 @@ class CollpaseTest extends Component {
 
     saveFeedback = (e) => {
         e.preventDefault();
-        console.log(this.state.schedules)
-        
+
         let username = fire.auth().currentUser.uid
         const ref = fire.database().ref(username)
         const scheduleRef = ref.child("schedule")
         scheduleRef.set(this.state.schedules).then((u) => {
-            console.log('Setting has been updated successfully!')
             this.setState({
                 message: 'Setting has been updated successfully!'
             })
@@ -75,42 +57,47 @@ class CollpaseTest extends Component {
     }
 
     render() {
-        console.log('sate', this.state.schedules)
+        console.log('sate', this.state.todaySchedules)
         return (
             <div className="row center">
                 <div className="col-md-6">
                     <span className="date-header">Date: {this.state.selectedDate.getDate()}</span>
                     <div>
-                        <Accordion defaultActiveKey={1}>
+                        <Accordion defaultActiveKey={0}>
                             {
-                                this.state.todaySchedules.map((obj, idx) => {
-                                    return (
-                                        <Card>
-                                            <Accordion.Toggle as={Card.Header} eventKey={idx}>
-                                                {obj.title}
-                                            </Accordion.Toggle>
-                                            <Accordion.Collapse eventKey={idx}>
-                                                <Card.Body>
-                                                    <div>
-                                                        <p>Start Time: {obj.startDate}</p>
-                                                    </div>
-                                                    <div>
-                                                        <p>End Time: {obj.endDate}</p>
-                                                    </div>
-                                                    <div>
-                                                        <Form className="none-style">
-                                                            <Form.Group controlId="exampleForm.ControlTextarea1">
-                                                                <Form.Label>Personal Feedback</Form.Label>
-                                                                <Form.Control onChange={(e) => this.handleFeedbackChange(e, idx)} value={obj.feedback} as="textarea" rows="3" />
-                                                            </Form.Group>
-                                                            <button type="submit" onClick={this.saveFeedback} className="btn btn-primary">Save Feedback</button>
-                                                        </Form>
-                                                    </div>
-                                                </Card.Body>
-                                            </Accordion.Collapse>
-                                        </Card>
-
-                                    )
+                                this.state.schedules.map((obj, idx) => {
+                                    // filter out the current date schedule
+                                    if (new Date(obj.startDate).getDate() === this.state.selectedDate.getDate()) {
+                                        return (
+                                            <Card>
+                                                <Accordion.Toggle as={Card.Header} eventKey={idx}>
+                                                <span class="glyphicon glyphicon-chevron-down"></span>{obj.title}
+                                                </Accordion.Toggle>
+                                                <Accordion.Collapse eventKey={idx}>
+                                                    <Card.Body>
+                                                        <div>
+                                                            <p>Start Time: {obj.startDate}</p>
+                                                        </div>
+                                                        <div>
+                                                            <p>End Time: {obj.endDate}</p>
+                                                        </div>
+                                                        <div>
+                                                            <Form className="none-style">
+                                                                <Form.Group controlId="exampleForm.ControlTextarea1">
+                                                                    <Form.Label>Personal Feedback</Form.Label>
+                                                                    <Form.Control onChange={(e) => this.handleFeedbackChange(e, idx)} value={obj.feedback} as="textarea" rows="3" />
+                                                                </Form.Group>
+                                                                <button type="submit" onClick={this.saveFeedback} className="btn btn-primary">Save Feedback</button>
+                                                            </Form>
+                                                        </div>
+                                                    </Card.Body>
+                                                </Accordion.Collapse>
+                                            </Card>
+    
+                                        )
+                                    } else {
+                                        return <div/>
+                                    }
                                 })
                             }
                         </Accordion>
