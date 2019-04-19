a45ef1e9607dcf322270f94092fd2adc6299632c
unknown <ykim164@illinois.edu>
2019-04-18 22:41:43 -0500

    remove api key web-app

---
+++ breaking_bad/web_app/src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyCSoc86zMDGJjRwTzjTnU2UFKc96VQlVAo",
+    authDomain: "breaking-bad-b34cc.firebaseapp.com",
+    databaseURL: "https://breaking-bad-b34cc.firebaseio.com",
+    projectId: "breaking-bad-b34cc",
+    storageBucket: "breaking-bad-b34cc.appspot.com",
+    messagingSenderId: "614753134089"
+};
+
+export default config;
--- breaking_bad/web_app/src/config/Fire.js
+++ breaking_bad/web_app/src/config/Fire.js
@@ -1,13 +1,5 @@
 import firebase from 'firebase';
-
-const config = {
-    apiKey: "AIzaSyCSoc86zMDGJjRwTzjTnU2UFKc96VQlVAo",
-    authDomain: "breaking-bad-b34cc.firebaseapp.com",
-    databaseURL: "https://breaking-bad-b34cc.firebaseio.com",
-    projectId: "breaking-bad-b34cc",
-    storageBucket: "breaking-bad-b34cc.appspot.com",
-    messagingSenderId: "614753134089"
-};
+import config from '../config'
 
 const fire = firebase.initializeApp(config);
 export default fire;
